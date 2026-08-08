9223d486afb948361574d3886922d2ee3e0e992b
jasminecristy005-droid <jasminecristy005@gmail.com>
2026-08-08 22:52:22 +0800

    Add files via upload

---
+++ Brew-Bliss-Landing-Page/js/script.js
@@ -0,0 +1,45 @@
+console.log("Brew & Bliss Loaded");
+
+const form = document.getElementById("coffee-form");
+const message = document.getElementById("form-message");
+
+form.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+
+    message.textContent = "Submitting...";
+
+    const formData = new FormData(form);
+
+    try {
+
+        const response = await fetch(form.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+
+        if (response.ok) {
+
+            window.location.href =
+                "https://jasmincristy005-droid.github.io/brew-bliss-landing-page/thankyou.html";
+
+        } else {
+
+            message.textContent =
+                "Something went wrong. Please try again.";
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        message.textContent =
+            "Unable to submit the form. Please try again.";
+
+    }
+
+});
